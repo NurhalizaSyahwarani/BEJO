9b1de44b8cad5a77704068b8ce1f3202afb2524b
Kelompokbejo <106146251+Kelompokbejo@users.noreply.github.com>
2022-06-07 11:05:42 +0700

    Update logam-tembaga.cpp
    menambahkan  dari float Cux[], Cuy[], Cuz[]; sampai  float rB_Sx = sin((  )*pi/ 0,0;

--- logam-tembaga.cpp
+++ logam-tembaga.cpp
@@ -11,4 +11,28 @@ int main (){
     float pi = 3.14;
     int counter = 0;
     
-    // deklarasi molekul tembaga
+    // deklarasi molekul tembaga(II) sulfat
+    float Cux[], Cuy[], Cuz[];
+    float Sx[], Sy[], Sz[];
+    float Ox[], Oy[], Oz[];
+    float Ox[], Oy[], Oz[];
+    float Ox[], Oy[], Oz[];
+    float Ox[], Oy[], Oz[];
+    
+    /*       O
+             |
+       Cu    S 
+          // | \\
+          O  O  O
+          molekul tembaga(II) sulfat itu bentuk rigid
+    */
+    
+    float rB_Cux = 0,0;
+    float rB_Cuy = 0,0;
+    float rB_Cuz = 0,0;
+    
+    float rB_Sx = sin((  )*pi/ 0,0;
+    
+          
+          
+    
