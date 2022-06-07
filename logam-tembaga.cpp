1bb3406b8e402d66d5fdaeb86bafee2e675dc0b3
Kelompokbejo <106146251+Kelompokbejo@users.noreply.github.com>
2022-06-07 11:22:43 +0700

    Update logam-tembaga.cpp
    edit sampai for(int c = 0; c < 5; c++){

--- logam-tembaga.cpp
+++ logam-tembaga.cpp
@@ -31,7 +31,30 @@ int main (){
     float rB_Cuy = 0,0;
     float rB_Cuz = 0,0;
     
-    float rB_Sx = sin((  )*pi/ 0,0;
+    float rB_Sx = 0,0;
+    float rB_Sy = 0,0;
+    float rB_Sz = 0,0;
+    
+    float rB_O1x = sin((  /  )*pi/   ) *    ;
+    float rB_O1y = 0.0;
+    float rB_O1z = cos((  /  )*pi/ ) * ;
+
+    float rB_O2x = - rB_O1x;
+    float rB_O2y = rB_O1y;
+    float rB_O2z = rB_O1z;
+
+    float rB_O3x = 0.0;
+    float rB_O3y = cos((  /  )*pi/  ) *   ;
+    float rB_O3z = sin((  /  )*pi/  ) *  ;
+
+    float rB_O4x = rB_O3x;
+    float rB_O4y = rB_O3y;
+    float rB_O4z = - rB_O3z;
+
+    // iterasi untuk setiap molekul tembaga(II) sulfat
+    for(int a = 0; a < 5; a++){
+        for(int b = 0; b < 5; b++){
+            for(int c = 0; c < 5; c++){
     
           
           
